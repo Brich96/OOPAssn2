b25d16ea52dcdcfded915469a71c33bf25b1730b
ben <benstargate@gmail.com>
2022-05-19 17:10:23 -0600

    Initial display done

--- src/main/kotlin/TrackerViewHelper.kt
+++ src/main/kotlin/TrackerViewHelper.kt
@@ -1,3 +1,17 @@
 class TrackerViewHelper {
+//    var shipmentID: String
+//    var shipmentTotes
+//    var shipmentUpateHistory
+//    var expectedShipmentDeliveryDate
+//    var shipmentStatus
+
+    fun trackShipment(id: String) {
+        // TODO: implement
+    }
+
+    fun stopTracking() {
+
+    }
+
 
 }
